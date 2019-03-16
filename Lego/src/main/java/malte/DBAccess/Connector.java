e30f27cce3549b3da71fa0210daabb42c1f91288
MalteMagnussen <maltehviidmagnussen@gmail.com>
2019-03-16 19:35:58 +0100

    Fixed DB and DBConnector
    Required version update and Timezone property

--- Lego/src/main/java/malte/DBAccess/Connector.java
+++ Lego/src/main/java/malte/DBAccess/Connector.java
@@ -3,28 +3,37 @@ package malte.DBAccess;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 /**
- The purpose of Connector is to...
-
- @author kasper
+ * The purpose of Connector is to...
+ *
+ * @author kasper
  */
 public class Connector {
 
-    private static final String URL = "jdbc:mysql://localhost/useradmin";
+    private static final String URL = "jdbc:mysql://localhost:3306/useradmin";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
 
     private static Connection singleton;
 
-    public static void setConnection( Connection con ) {
+    public static void setConnection(Connection con) {
         singleton = con;
     }
 
     public static Connection connection() throws ClassNotFoundException, SQLException {
-        if ( singleton == null ) {
-            Class.forName( "com.mysql.cj.jdbc.Driver" );
-            singleton = DriverManager.getConnection( URL, USERNAME, PASSWORD );
+        if (singleton == null) {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Properties props = new Properties();
+            props.put("user", USERNAME);
+            props.put("password", PASSWORD);
+            props.put("allowMultiQueries", true);
+            props.put("useUnicode", true);
+            props.put("useJDBCCompliantTimezoneShift", true);
+            props.put("useLegacyDatetimeCode", false);
+            props.put("serverTimezone", "CET");
+            singleton = DriverManager.getConnection(URL, props);
         }
         return singleton;
     }
--- Lego/src/test/java/malte/DBAccess/UserMapperTest.java
+++ Lego/src/test/java/malte/DBAccess/UserMapperTest.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 import malte.FunctionLayer.LoginSampleException;
 import malte.FunctionLayer.User;
 import org.junit.Test;
@@ -28,60 +29,67 @@ public class UserMapperTest {
     public void setUp() {
         try {
             // awoid making a new connection for each test
-            if ( testConnection == null ) {
-                String url = String.format( "jdbc:mysql://%s:3306/%s", HOST, DBNAME );
-                Class.forName( "com.mysql.cj.jdbc.Driver" );
-
-                testConnection = DriverManager.getConnection( url, USER, USERPW );
+            if (testConnection == null) {
+                String url = String.format("jdbc:mysql://%s:3306/%s", HOST, DBNAME);
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                Properties props = new Properties();
+                props.put("user", USER);
+                props.put("password", USERPW);
+                props.put("allowMultiQueries", true);
+                props.put("useUnicode", true);
+                props.put("useJDBCCompliantTimezoneShift", true);
+                props.put("useLegacyDatetimeCode", false);
+                props.put("serverTimezone", "CET");
+                testConnection = DriverManager.getConnection(url, props);
                 // Make mappers use test 
-                Connector.setConnection( testConnection );
+                Connector.setConnection(testConnection);
             }
             // reset test database
-            try ( Statement stmt = testConnection.createStatement() ) {
-                stmt.execute( "drop table if exists Users" );
-                stmt.execute( "create table Users like UsersTest" );
-                stmt.execute( "insert into Users select * from UsersTest" );
+            try (Statement stmt = testConnection.createStatement()) {
+                stmt.execute("drop table if exists Users");
+                stmt.execute("create table Users like UsersTest");
+                stmt.execute("insert into Users select * from UsersTest");
             }
 
-        } catch ( ClassNotFoundException | SQLException ex ) {
+        } catch (ClassNotFoundException | SQLException ex) {
             testConnection = null;
-            System.out.println( "Could not open connection to database: " + ex.getMessage() );
+            System.out.println("Could not open connection to database: " + ex.getMessage());
         }
     }
 
     @Test
     public void testSetUpOK() {
         // Just check that we have a connection.
-        assertNotNull( testConnection );
+        assertNotNull(testConnection);
     }
 
     @Test
     public void testLogin01() throws LoginSampleException {
         // Can we log in
-        User user = UserMapper.login( "jens@somewhere.com", "jensen" );
-        assertTrue( user != null );
+        User user = UserMapper.login("jens@somewhere.com", "jensen");
+        assertTrue(user != null);
     }
 
-    @Test( expected = LoginSampleException.class )
+    @Test(expected = LoginSampleException.class)
     public void testLogin02() throws LoginSampleException {
         // We should get an exception if we use the wrong password
-        User user = UserMapper.login( "jens@somewhere.com", "larsen" );
+        User user = UserMapper.login("jens@somewhere.com", "larsen");
     }
 
     @Test
     public void testLogin03() throws LoginSampleException {
         // Jens is supposed to be a customer
-        User user = UserMapper.login( "jens@somewhere.com", "jensen" );
-        assertEquals( "customer", user.getRole() );
+        User user = UserMapper.login("jens@somewhere.com", "jensen");
+        assertEquals("customer", user.getRole());
     }
 
     @Test
     public void testCreateUser01() throws LoginSampleException {
         // Can we create a new user - Notice, if login fails, this will fail
         // but so would login01, so this is OK
-        User original = new User( "king@kong.com", "uhahvorhemmeligt", "konge" );
-        UserMapper.createUser( original );
-        User retrieved = UserMapper.login( "king@kong.com", "uhahvorhemmeligt" );
-        assertEquals( "konge", retrieved.getRole() );
+        User original = new User("king@kong.com", "uhahvorhemmeligt", "konge");
+        UserMapper.createUser(original);
+        User retrieved = UserMapper.login("king@kong.com", "uhahvorhemmeligt");
+        assertEquals("konge", retrieved.getRole());
     }
 }
