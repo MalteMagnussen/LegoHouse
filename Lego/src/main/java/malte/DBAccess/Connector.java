e8e9a1781e1790c2086eb4de22b2625cdce874dc
MalteMagnussen <maltehviidmagnussen@gmail.com>
2019-03-16 19:13:08 +0100

    Fixed DB connection

--- Lego/src/main/java/malte/DBAccess/Connector.java
+++ Lego/src/main/java/malte/DBAccess/Connector.java
@@ -11,9 +11,9 @@ import java.sql.SQLException;
  */
 public class Connector {
 
-    private static final String URL = "jdbc:mysql://46.101.253.149:3306/useradmin";
-    private static final String USERNAME = "doorkeeper";
-    private static final String PASSWORD = "bank3*andyouarein";
+    private static final String URL = "jdbc:mysql://localhost/useradmin";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
 
     private static Connection singleton;
 
--- Lego/src/test/java/malte/DBAccess/UserMapperTest.java
+++ Lego/src/test/java/malte/DBAccess/UserMapperTest.java
@@ -1,68 +1,87 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package malte.DBAccess;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import malte.FunctionLayer.LoginSampleException;
 import malte.FunctionLayer.User;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 
-/**
- *
- * @author Malte
- */
 public class UserMapperTest {
-    
-    public UserMapperTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
+//    Test date in the UsersTest table
+//    INSERT INTO `UsersTest` VALUES 
+//    (1,'jens@somewhere.com','jensen','customer'),
+//    (2,'ken@somewhere.com','kensen','customer'),
+//    (3,'robin@somewhere.com','batman','employee'),
+//    (4,'someone@nowhere.com','sesam','customer');
+
+    private static Connection testConnection;
+    private static String USER = "root";
+    private static String USERPW = "root";
+    private static String DBNAME = "useradminTest";
+    private static String HOST = "localhost";
+
     @Before
     public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
+        try {
+            // awoid making a new connection for each test
+            if ( testConnection == null ) {
+                String url = String.format( "jdbc:mysql://%s:3306/%s", HOST, DBNAME );
+                Class.forName( "com.mysql.cj.jdbc.Driver" );
+
+                testConnection = DriverManager.getConnection( url, USER, USERPW );
+                // Make mappers use test 
+                Connector.setConnection( testConnection );
+            }
+            // reset test database
+            try ( Statement stmt = testConnection.createStatement() ) {
+                stmt.execute( "drop table if exists Users" );
+                stmt.execute( "create table Users like UsersTest" );
+                stmt.execute( "insert into Users select * from UsersTest" );
+            }
+
+        } catch ( ClassNotFoundException | SQLException ex ) {
+            testConnection = null;
+            System.out.println( "Could not open connection to database: " + ex.getMessage() );
+        }
     }
 
-    /**
-     * Test of createUser method, of class UserMapper.
-     */
     @Test
-    public void testCreateUser() throws Exception {
-        System.out.println("createUser");
-        User user = null;
-        UserMapper.createUser(user);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testSetUpOK() {
+        // Just check that we have a connection.
+        assertNotNull( testConnection );
     }
 
-    /**
-     * Test of login method, of class UserMapper.
-     */
     @Test
-    public void testLogin() throws Exception {
-        System.out.println("login");
-        String email = "";
-        String password = "";
-        User expResult = null;
-        User result = UserMapper.login(email, password);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testLogin01() throws LoginSampleException {
+        // Can we log in
+        User user = UserMapper.login( "jens@somewhere.com", "jensen" );
+        assertTrue( user != null );
     }
-    
+
+    @Test( expected = LoginSampleException.class )
+    public void testLogin02() throws LoginSampleException {
+        // We should get an exception if we use the wrong password
+        User user = UserMapper.login( "jens@somewhere.com", "larsen" );
+    }
+
+    @Test
+    public void testLogin03() throws LoginSampleException {
+        // Jens is supposed to be a customer
+        User user = UserMapper.login( "jens@somewhere.com", "jensen" );
+        assertEquals( "customer", user.getRole() );
+    }
+
+    @Test
+    public void testCreateUser01() throws LoginSampleException {
+        // Can we create a new user - Notice, if login fails, this will fail
+        // but so would login01, so this is OK
+        User original = new User( "king@kong.com", "uhahvorhemmeligt", "konge" );
+        UserMapper.createUser( original );
+        User retrieved = UserMapper.login( "king@kong.com", "uhahvorhemmeligt" );
+        assertEquals( "konge", retrieved.getRole() );
+    }
 }
