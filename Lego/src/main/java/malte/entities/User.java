3a243453b8eb1e242c2bac4459e010feca3820c5
MalteMagnussen <maltehviidmagnussen@gmail.com>
2019-03-17 10:52:48 +0100

    Update User

--- Lego/src/main/java/malte/entities/User.java
+++ Lego/src/main/java/malte/entities/User.java
@@ -1,52 +1,106 @@
 package malte.entities;
 
 /**
- * The purpose of User is to...
- * @author kasper
+ * User Entity Class.
+ * @author Malte
  */
 public class User {
 
-    public User( String email, String password, String role ) {
+    /**
+     * Fields
+     */
+    private String password, email, role;
+    private int id;
+
+    /**
+     * Constructor.
+     *
+     * @param email Users email
+     * @param password Users password
+     * @param role Users role
+     */
+    public User(String email, String password, String role) {
         this.email = email;
         this.password = password;
         this.role = role;
     }
 
-    private int id; // just used to demo retrieval of autogen keys in UserMapper
-    private String email;
-    private String password; // Should be hashed and secured
-    private String role;
-
-    public String getEmail() {
-        return email;
+    /**
+     * JavaBean
+     */
+    public User() {
     }
 
-    public void setEmail( String email ) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword( String password ) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole( String role ) {
-        this.role = role;
-    }
-
+    /**
+     * Get the value of id
+     *
+     * @return the value of id
+     */
     public int getId() {
         return id;
     }
 
-    public void setId( int id ) {
+    /**
+     * Set the value of id
+     *
+     * @param id new value of id
+     */
+    public void setId(int id) {
         this.id = id;
+    }
+
+    /**
+     * Get the value of email
+     *
+     * @return the value of email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @param email new value of email
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * Get the value of password
+     *
+     * @return the value of password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Set the value of password
+     *
+     * @param password new value of password
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * Get the value of role
+     *
+     * @return the value of role
+     */
+    public String getRole() {
+        return role;
+    }
+
+    /**
+     * Set the value of role
+     *
+     * @param role new value of role
+     */
+    public void setRole(String role) {
+        this.role = role;
     }
 
 }
