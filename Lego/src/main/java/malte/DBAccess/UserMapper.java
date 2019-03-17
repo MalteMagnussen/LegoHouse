baf2d7935b2441b4a77898636c18c26271bcaf7f
MalteMagnussen <maltehviidmagnussen@gmail.com>
2019-03-17 17:36:12 +0100

    Trying to fix exception

--- Lego/src/main/java/malte/DBAccess/UserMapper.java
+++ Lego/src/main/java/malte/DBAccess/UserMapper.java
@@ -5,68 +5,73 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 import malte.FunctionLayer.CustomException;
+import malte.entities.Order;
 import malte.entities.User;
 
 public class UserMapper {
 
     /**
      * Create User Method.
-     * 
+     *
      * Inputs a User into the SQL database.
-     * 
+     *
      * @param user Entity
-     * @throws CustomException 
+     * @throws CustomException
      */
-    public static void createUser( User user ) throws CustomException {
+    public static void createUser(User user) throws CustomException {
         try {
             Connection con = Connector.connection();
             String SQL = "INSERT INTO Users (email, password, role) VALUES (?, ?, ?)";
-            PreparedStatement ps = con.prepareStatement( SQL, Statement.RETURN_GENERATED_KEYS );
-            ps.setString( 1, user.getEmail() );
-            ps.setString( 2, user.getPassword() );
-            ps.setString( 3, user.getRole() );
+            PreparedStatement ps = con.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, user.getEmail());
+            ps.setString(2, user.getPassword());
+            ps.setString(3, user.getRole());
             ps.executeUpdate();
             ResultSet ids = ps.getGeneratedKeys();
             ids.next();
-            int id = ids.getInt( 1 );
-            user.setId( id );
-        } catch ( SQLException | ClassNotFoundException ex ) {
-            throw new CustomException( ex.getMessage() );
+            int id = ids.getInt(1);
+            user.setId(id);
+        } catch (SQLException | ClassNotFoundException ex) {
+            throw new CustomException(ex.getMessage());
         }
     }
 
     /**
      * Login Method.
-     * 
-     * Pulls a User entity from the SQL if the User input correct info into the form.
-     * Else throws an exception and returns User to the index page.
-     * 
+     *
+     * Pulls a User entity from the SQL if the User input correct info into the
+     * form. Else throws an exception and returns User to the index page.
+     *
      * @param email Users email
      * @param password Users password
      * @return User entity
-     * @throws CustomException 
+     * @throws CustomException
      */
-    public static User login( String email, String password ) throws CustomException {
+    public static User login(String email, String password) throws CustomException {
         try {
             Connection con = Connector.connection();
             String SQL = "SELECT id, role FROM Users "
                     + "WHERE email=? AND password=?";
-            PreparedStatement ps = con.prepareStatement( SQL );
-            ps.setString( 1, email );
-            ps.setString( 2, password );
+            PreparedStatement ps = con.prepareStatement(SQL);
+            ps.setString(1, email);
+            ps.setString(2, password);
             ResultSet rs = ps.executeQuery();
-            if ( rs.next() ) {
-                String role = rs.getString( "role" );
-                int id = rs.getInt( "id" );
-                User user = new User( email, password, role );
-                user.setOrders(OrderMapper.getOrders(user));
-                user.setId( id );
+            if (rs.next()) {
+                String role = rs.getString("role");
+                int id = rs.getInt("id");
+                User user = new User(email, password, role);
+                List<Order> orders = OrderMapper.getOrders(user);
+                if (orders != null && !orders.isEmpty()) {
+                    user.setOrders(orders);
+                }
+                user.setId(id);
                 return user;
             } else {
-                throw new CustomException( "Could not validate user" );
+                throw new CustomException("Could not validate user");
             }
-        } catch ( ClassNotFoundException | SQLException ex ) {
+        } catch (ClassNotFoundException | SQLException ex) {
             throw new CustomException(ex.getMessage());
         }
     }
