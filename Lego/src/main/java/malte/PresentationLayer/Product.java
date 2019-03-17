f8ecccce130f21d507f0f8ff6a9c1131fc95c7e0
MalteMagnussen <maltehviidmagnussen@gmail.com>
2019-03-17 14:10:09 +0100

    Update Product.java

--- Lego/src/main/java/malte/PresentationLayer/Product.java
+++ Lego/src/main/java/malte/PresentationLayer/Product.java
@@ -5,6 +5,7 @@
  */
 package malte.PresentationLayer;
 
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -27,6 +28,9 @@ public class Product extends Command {
             // Adds an Order to the SQL.
             case "addProduct": {
                 return addProduct(request);
+            }
+            case "order": {
+                return order(request);
             }
             
         }
@@ -56,6 +60,23 @@ public class Product extends Command {
         Order order = LogicFacade.createOrder(id, length, width, height);
         HttpSession session = request.getSession();
         session.setAttribute("order", order);
+        User user = (User) session.getAttribute("user");
+        return user.getRole() + "page";
+    }
+
+    private String order(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        
+        String date = (String) request.getParameter("date");
+        
+        List<Order> orders = (List<Order>) session.getAttribute("orders");
+        
+        for (Order order: orders){
+            if (order.getDate().equals(date)){
+                session.setAttribute("order", order);
+            }
+        }
+        
         User user = (User) session.getAttribute("user");
         return user.getRole() + "page";
     }
