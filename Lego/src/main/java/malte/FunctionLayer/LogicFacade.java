5561d3004e3e6cc7cb3fa47c4e40c2720683c460
MalteMagnussen <maltehviidmagnussen@gmail.com>
2019-03-17 10:54:14 +0100

    Update Facade

--- Lego/src/main/java/malte/FunctionLayer/LogicFacade.java
+++ Lego/src/main/java/malte/FunctionLayer/LogicFacade.java
@@ -3,13 +3,30 @@ package malte.FunctionLayer;
 import malte.entities.User;
 import malte.DBAccess.UserMapper;
 
-
+/**
+ * Facade Class.
+ * @author Malte
+ */
 public class LogicFacade {
 
+    /**
+     * User Login Facade.
+     * @param email Users email.
+     * @param password Users password.
+     * @return User entity.
+     * @throws LoginSampleException 
+     */
     public static User login( String email, String password ) throws LoginSampleException {
         return UserMapper.login( email, password );
     } 
 
+    /**
+     * Create User Facade.
+     * @param email Users email.
+     * @param password Users password.
+     * @return User entity.
+     * @throws LoginSampleException 
+     */
     public static User createUser( String email, String password ) throws LoginSampleException {
         User user = new User(email, password, "customer");
         UserMapper.createUser( user );
--- Lego/src/main/java/malte/FunctionLayer/LoginSampleException.java
+++ Lego/src/main/java/malte/FunctionLayer/LoginSampleException.java
@@ -1,5 +1,9 @@
 package malte.FunctionLayer;
 
+/**
+ * Custom Exception.
+ * @author Malte
+ */
 public class LoginSampleException extends Exception {
 
     public LoginSampleException(String msg) {
