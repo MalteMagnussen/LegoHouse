b96b09baa5757d8c6fa595dbc27cfd5fcf9d37cb
MalteMagnussen <maltehviidmagnussen@gmail.com>
2019-03-17 11:00:57 +0100

    Update UnknownCommand.java

--- Lego/src/main/java/malte/PresentationLayer/UnknownCommand.java
+++ Lego/src/main/java/malte/PresentationLayer/UnknownCommand.java
@@ -4,6 +4,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import malte.FunctionLayer.LoginSampleException;
 
+/**
+ * Unknown Command.
+ * Throws Exception.
+ * @author Malte
+ */
 public class UnknownCommand extends Command {
 
     @Override
