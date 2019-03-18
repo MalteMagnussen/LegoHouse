9d898fae5cd9f105e6c6e5b2d8ea6c591e68c278
MalteMagnussen <maltehviidmagnussen@gmail.com>
2019-03-18 17:39:33 +0100

    Trying to make Bill of Materials.

---
+++ Lego/src/main/java/malte/FunctionLayer/BOMLogic.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package malte.FunctionLayer;
+
+import malte.entities.Row;
+
+/**
+ *
+ * @author Malte
+ */
+public class BOMLogic
+{
+    
+    public static Row getRow(int length)
+    {
+        Row row = new Row();
+        int fours = length / 4;
+        row.setFours(fours);
+        length = length % 4;
+        if (length >= 2)
+        {
+            int twos = length / 2;
+            row.setTwos(twos);
+            length = length % 2;
+            if (length >= 1)
+            {
+                int ones = length;
+                row.setOnes(ones);
+            }
+        }
+        
+        return row;
+    }
+    
+}
---
+++ Lego/src/main/java/malte/entities/Row.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package malte.entities;
+
+/**
+ *
+ * @author Malte
+ */
+public class Row {
+    private int fours;
+    private int twos;
+    private int ones;
+
+    public Row() {
+    }
+
+    public int getFours() {
+        return fours;
+    }
+
+    public void setFours(int fours) {
+        this.fours = fours;
+    }
+
+    public int getTwos() {
+        return twos;
+    }
+
+    public void setTwos(int twos) {
+        this.twos = twos;
+    }
+
+    public int getOnes() {
+        return ones;
+    }
+
+    public void setOnes(int ones) {
+        this.ones = ones;
+    }
+    
+    
+}
