6819e9b94eed5c2a5cc34fc9a7313fbbcb3e8068
MalteMagnussen <maltehviidmagnussen@gmail.com>
2019-03-18 17:48:54 +0100

    Update BOMLogic.java

--- Lego/src/main/java/malte/FunctionLayer/BOMLogic.java
+++ Lego/src/main/java/malte/FunctionLayer/BOMLogic.java
@@ -17,20 +17,28 @@ public class BOMLogic
      * Makes a single Row of bricks.
      * A side consists of Height X Rows of bricks.
      * A House consists of 4 sides.
-     * @param length
+     * 
+     * 
+     * @param length Has to be at least 8. Maybe 10. We'll see.
      * @return 
      */
     public static Row getRow(int length)
     {
         Row row = new Row();
+        
+        /* Fours */
         int fours = length / 4;
         row.setFours(fours);
         length = length % 4;
+        
+        /* Twos */
         if (length >= 2)
         {
             int twos = length / 2;
             row.setTwos(twos);
             length = length % 2;
+            
+            /* Ones */
             if (length >= 1)
             {
                 int ones = length;
