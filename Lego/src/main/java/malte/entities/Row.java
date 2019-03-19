6c8033fdd696b518557dac86772ea85ce45f9e64
MalteMagnussen <maltehviidmagnussen@gmail.com>
2019-03-19 18:48:16 +0100

    Update Row.java

--- Lego/src/main/java/malte/entities/Row.java
+++ Lego/src/main/java/malte/entities/Row.java
@@ -6,40 +6,80 @@
 package malte.entities;
 
 /**
+ * Row entity class. Should fulfill bean standards.
  *
  * @author Malte
  */
-public class Row {
+public class Row
+{
+
+    // Fields
     private int fours;
     private int twos;
     private int ones;
 
-    public Row() {
+    public Row()
+    {
     }
 
-    public int getFours() {
-        return fours;
-    }
-
-    public void setFours(int fours) {
-        this.fours = fours;
-    }
-
-    public int getTwos() {
-        return twos;
-    }
-
-    public void setTwos(int twos) {
-        this.twos = twos;
-    }
-
-    public int getOnes() {
+    /**
+     * Get the value of ones
+     *
+     * @return the value of ones
+     */
+    public int getOnes()
+    {
         return ones;
     }
 
-    public void setOnes(int ones) {
+    /**
+     * Set the value of ones
+     *
+     * @param ones new value of ones
+     */
+    public void setOnes(int ones)
+    {
         this.ones = ones;
     }
-    
-    
+
+    /**
+     * Get the value of fours
+     *
+     * @return the value of fours
+     */
+    public int getFours()
+    {
+        return fours;
+    }
+
+    /**
+     * Set the value of fours
+     *
+     * @param fours new value of fours
+     */
+    public void setFours(int fours)
+    {
+        this.fours = fours;
+    }
+
+    /**
+     * Get the value of twos
+     *
+     * @return the value of twos
+     */
+    public int getTwos()
+    {
+        return twos;
+    }
+
+    /**
+     * Set the value of twos
+     *
+     * @param twos new value of twos
+     */
+    public void setTwos(int twos)
+    {
+        this.twos = twos;
+    }
+
 }
