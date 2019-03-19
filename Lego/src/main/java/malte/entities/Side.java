1455738159d3a979e7a9aec725ba5cd50b773630
MalteMagnussen <maltehviidmagnussen@gmail.com>
2019-03-19 14:54:52 +0100

    Update Side.java

--- Lego/src/main/java/malte/entities/Side.java
+++ Lego/src/main/java/malte/entities/Side.java
@@ -18,29 +18,7 @@ public class Side
     private boolean window;
     private boolean door;
     private int height;
-    private String name;
 
-    /**
-     * Get the value of name
-     *
-     * @return the value of name
-     */
-    public String getName()
-    {
-        return name;
-    }
-
-    /**
-     * Set the value of name
-     *
-     * @param name new value of name
-     */
-    public void setName(String name)
-    {
-        this.name = name;
-    }
-
-    
     /**
      * Get the value of height
      *
